7191fcea21ca3a78a44c3715bea74e5156477381
Andrea Ziccardi <andryziccardi@gmail.com>
2025-10-29 14:36:48 +0100

    Feature: Aggiunta pagina Lavori con struttura base

---
+++ lavori.js
@@ -0,0 +1,8 @@
+// Logica per la pagina Lavori
+// Da implementare in futuro
+
+console.log('Pagina Lavori caricata');
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOM caricato - Pagina Lavori pronta');
+});
